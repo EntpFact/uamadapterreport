2ba6aee94a509861d87606cd1ac15c92e23d8877
marudhu18 <marudhupandian.s@fyndna.com>
2025-06-13 00:45:05 +0530

    new change

--- src/main/java/com/hdfcbank/uamadapterreport/util/Constant.java
+++ src/main/java/com/hdfcbank/uamadapterreport/util/Constant.java
@@ -2,4 +2,7 @@ package com.hdfcbank.uamadapterreport.util;
 
 public class Constant {
     public static final String SUCCESS = "success";
+    private Constant() {
+        // Private constructor to prevent instantiation of utility class
+    }
 }
